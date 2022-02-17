ccba0d3393e50ec5229d3d5f4f98d62beb9c77f0
Marijn van Vliet <w.m.vanvliet@gmail.com>
2022-02-17 09:36:56 +0200

    Tweaks to the layout

--- random_lines.js
+++ random_lines.js
@@ -5,7 +5,7 @@ canvas.width = window.innerWidth;
 const ctx = canvas.getContext('2d');
 ctx.fillStyle = '#ffffff';
 ctx.fillRect(0, 0, canvas.width, canvas.height);
-ctx.strokeStyle = '#ddd';
+ctx.strokeStyle = '#eee';
 //ctx.strokeStyle = 'white';
 
 function drawTraces() {
